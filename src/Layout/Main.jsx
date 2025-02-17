e3f219ecae5c64f7e9e6876a3d25429774a1e12f
contactmizan <contactmizan1206@gmail.com>
2025-02-18 02:44:54 +0600

    added navbar and footer

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../Pages/Home/Shared/Footer";
+import Navbar from "../Pages/Home/Shared/Navbar";
 
 
 const Main = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 }
